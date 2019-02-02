1c2feb86eef941c0f3411652ae19f0bb5cff538b
Andrew Anischik <anischik.29@gmail.com>
2019-02-02 18:34:49 +0300

    feat: added logs

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -1,50 +1,42 @@
 package server;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class HttpServer
-    implements Runnable{
+public class HttpServer {
+    Dotenv dotenv = Dotenv.configure().directory("./").load();
+
     private static boolean running = false;
     private ServerSocket serverSocket = null;
     private Database database;
 
     private ConnectionManager connectionsManager = null;
 
-    public HttpServer() throws IOException {
-        serverSocket = new ServerSocket();
-        connectionsManager = new ConnectionManager();
+    public HttpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port, Integer.valueOf(dotenv.get("BACKLOG")), InetAddress.getLocalHost());
         database = new Database();
     }
 
-    public static boolean isRunning(){
+    public static boolean isRunning() {
         return running;
     }
 
-    public void stop(){
-        if(!Thread.currentThread().isInterrupted()){
+    public void stop() {
+        if (!Thread.currentThread().isInterrupted()) {
             Thread.currentThread().interrupt();
         }
 
-        for(HttpConnection session: connectionsManager.getConnection()) {
-            session.stop();
-        }
-
-        connectionsManager.removeAll();
-    }
-
-
-    @Override
-    public void run() {
-        runServer();
     }
 
     private void runServer() {
         while (running) {
             try {
                 Socket client = serverSocket.accept();
-                HttpConnection session = new HttpConnection(this,client);
+                HttpConnection session = new HttpConnection(this, client);
                 connectionsManager.add(session);
                 new Thread(session).start();
             } catch (IOException e) {
--- src/main/java/ui/UserUI.java
+++ src/main/java/ui/UserUI.java
@@ -1,56 +1,55 @@
 package ui;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import server.HttpServer;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-public class UserUI implements Initializable {
+public class UserUI {
     private HttpServer httpServer;
 
     @FXML
     private TextField serverPort;
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+    @FXML
+    private TextArea logArea;
 
     @FXML
-    private void run() {
+    private void start() {
         if (isValidPort()) {
-            System.out.println("Is valid");
             if (httpServer == null) {
                 try {
-                    httpServer = new HttpServer();
-                    new Thread(httpServer).start();
+                    httpServer = new HttpServer(Integer.valueOf(serverPort.getText()));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else {
-
+                writeLog("Server is already running.");
             }
         } else {
-            //сервер уже запущен
+            writeLog("Port is not valid.");
         }
     }
 
     @FXML
     private void stop() {
-        if(HttpServer.isRunning()) {
+        if (HttpServer.isRunning()) {
             httpServer.stop();
             httpServer = null;
         } else {
-            //сервер уже остановлен
+            writeLog("Server has already stopped.");
         }
     }
 
     private boolean isValidPort() {
         Pattern pattern = Pattern.compile("(([0-9]{1,4})|([1-5][0-9]{4})|(6[0-4][0-9]{3})|(65[0-4][0-9]{2})|(655[0-2][0-9])|(6553[0-5]))");
         return pattern.matcher(serverPort.getText()).matches();
+    }
+
+    private void writeLog(String message){
+        logArea.appendText(message.concat("\n"));
     }
 }
