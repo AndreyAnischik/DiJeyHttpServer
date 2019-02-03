bc25897258be46d650ea5b0ae2d85b7015741930
Andrew Anischik <anischik.29@gmail.com>
2019-02-03 10:38:53 +0300

    feat: got index page

--- src/main/java/server/HttpConnection.java
+++ src/main/java/server/HttpConnection.java
@@ -1,7 +1,9 @@
 package server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 public class HttpConnection implements Runnable {
     private HttpServer httpServer;
@@ -14,6 +16,52 @@ public class HttpConnection implements Runnable {
 
     @Override
     public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedOutputStream dataOut = null;
+        String file = null;
+
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            dataOut = new BufferedOutputStream(socket.getOutputStream());
+
+            String input = in.readLine();
+            StringTokenizer parse = new StringTokenizer(input);
+            String method = parse.nextToken().toUpperCase();
+
+            file = parse.nextToken().toLowerCase();
+
+
+            File sendingFile = new File("./", file.substring(1));
+            int fileLength = (int) sendingFile.length();
+            String content = getContentType(file);
+
+            byte[] fileData = readFileData(sendingFile, fileLength);
+
+            out.println("HTTP/1.1 200 OK");
+            out.println("Server: Java http server by DiJey");
+            out.println("Date: " + new Date());
+            out.println("Content-type: " + content);
+            out.println("Content-length: " + fileLength);
+            out.println();
+            out.flush();
+
+            dataOut.write(fileData, 0, fileLength);
+            dataOut.flush();
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+                dataOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void stop() {
@@ -25,5 +73,26 @@ public class HttpConnection implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private byte[] readFileData(File file, int fileLength) throws IOException {
+        FileInputStream fileIn = null;
+        byte[] fileData = new byte[fileLength];
+
+        try {
+            fileIn = new FileInputStream(file);
+            fileIn.read(fileData);
+        } finally {
+            if (fileIn != null) {
+                fileIn.close();
+            }
+        }
+
+        return fileData;
+
+    }
+
+    private String getContentType(String file) {
+        return "text/html";
     }
 }
