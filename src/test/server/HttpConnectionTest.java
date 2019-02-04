b6653a42b337bb5fd3929639b1366b0b33b16051
Andrew Anischik <anischik.29@gmail.com>
2019-02-04 22:01:22 +0300

    test: completed get method test

--- src/test/server/HttpConnectionTest.java
+++ src/test/server/HttpConnectionTest.java
@@ -3,13 +3,12 @@ package server;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.mockito.Mockito;
+
 import java.io.*;
 import java.net.Socket;
 
-import org.mockito.Mockito;
-
 import static org.junit.Assert.*;
-
 
 public class HttpConnectionTest {
     private HttpServer httpServer;
@@ -25,10 +24,35 @@ public class HttpConnectionTest {
     public void getTesting() throws IOException {
         final String REQUEST_CONTENT = "GET /index.html HTTP/1.1";
 
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
         Mockito.doReturn(new ByteArrayInputStream(REQUEST_CONTENT.getBytes())).when(socket).getInputStream();
-        Mockito.doReturn(Mockito.mock(OutputStream.class)).when(socket).getOutputStream();
+        Mockito.doReturn(baos).when(socket).getOutputStream();
 
         HttpConnection connection = new HttpConnection(httpServer, socket);
         connection.run();
+
+        String fileName = connection.getFileName();
+
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(Constants.CONTENT_DIRECTORY + fileName));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+                contentBuilder.append('\n');
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String initialContent = contentBuilder.toString();
+        String sendingContent = baos.toString();
+
+        assertTrue(sendingContent.contains("HTTP/1.1 " + Constants.OK));
+        assertTrue(sendingContent.contains("Content-type: text/html"));
+        assertTrue(sendingContent.contains("Content-length: " + initialContent.length()));
+        assertTrue(sendingContent.contains(initialContent));
     }
 }
