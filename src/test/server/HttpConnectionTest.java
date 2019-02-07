a20afd955318b41aa3db678c785482348980392b
Andrew Anischik <anischik.29@gmail.com>
2019-02-07 23:59:05 +0300

    test: refactored common method; added delete method testing

--- src/test/server/HttpConnectionTest.java
+++ src/test/server/HttpConnectionTest.java
@@ -30,10 +30,41 @@ public class HttpConnectionTest {
         Mockito.doReturn(baos).when(socket).getOutputStream();
 
         HttpConnection connection = new HttpConnection(httpServer, socket);
-        connection.run();
+        connection.handleResponse();
 
         String fileName = connection.getFileName();
 
+        String initialContent = getInitialContent(fileName);
+        String sendingContent = baos.toString();
+
+        assertTrue(sendingContent.contains("HTTP/1.1 " + Constants.OK));
+        assertTrue(sendingContent.contains("Content-type: text/html"));
+        assertTrue(sendingContent.contains("Content-length: " + initialContent.length()));
+        assertTrue(sendingContent.contains(initialContent));
+    }
+
+    @Test
+    public void deleteTesting() throws IOException {
+        final String REQUEST_CONTENT = "DELETE /some_path HTTP/1.1";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Mockito.doReturn(new ByteArrayInputStream(REQUEST_CONTENT.getBytes())).when(socket).getInputStream();
+        Mockito.doReturn(baos).when(socket).getOutputStream();
+
+        HttpConnection connection = new HttpConnection(httpServer, socket);
+        connection.handleResponse();
+
+        String initialContent = getInitialContent(Constants.NOT_IMPLEMENTED_PAGE);
+        String sendingContent = baos.toString();
+
+        assertTrue(sendingContent.contains("HTTP/1.1 " + Constants.NOT_IMPLEMENTED));
+        assertTrue(sendingContent.contains("Content-type: text/html"));
+        assertTrue(sendingContent.contains("Content-length: " + initialContent.length()));
+        assertTrue(sendingContent.contains(initialContent));
+    }
+
+    private String getInitialContent(String fileName){
         StringBuilder contentBuilder = new StringBuilder();
         try {
             BufferedReader in = new BufferedReader(new FileReader(Constants.CONTENT_DIRECTORY + fileName));
@@ -47,12 +78,6 @@ public class HttpConnectionTest {
             e.printStackTrace();
         }
 
-        String initialContent = contentBuilder.toString();
-        String sendingContent = baos.toString();
-
-        assertTrue(sendingContent.contains("HTTP/1.1 " + Constants.OK));
-        assertTrue(sendingContent.contains("Content-type: text/html"));
-        assertTrue(sendingContent.contains("Content-length: " + initialContent.length()));
-        assertTrue(sendingContent.contains(initialContent));
+        return contentBuilder.toString();
     }
 }
